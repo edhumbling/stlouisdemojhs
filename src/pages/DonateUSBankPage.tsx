0efef99bb1b7d16f25e05c76f7542fb9e646a1cb
Emmanuel D. Humbling <emmergraix@gmail.com>
2025-06-07 14:32:16 +0000

    Remove fixed footer from donation pages for a cleaner layout

--- src/pages/DonateUSBankPage.tsx
+++ src/pages/DonateUSBankPage.tsx
@@ -110,13 +110,6 @@ const DonateUSBankPage: React.FC = () => {
           </div>
         </div>
       </main>
-      
-      <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 text-center py-3 px-4 text-sm text-gray-600 z-50">
-        <div className="flex items-center justify-center gap-2">
-          <Shield className="text-green-500" size={16} />
-          <span>100% of your donation goes directly to St. Louis Demo JHS</span>
-        </div>
-      </div>
     </div>
   );
 };
