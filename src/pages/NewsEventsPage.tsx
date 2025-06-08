ba37d3194000e86192162f3297bc429eb3a633f8
Emmanuel D. Humbling <emmergraix@gmail.com>
2025-06-08 07:31:28 +0000

    Add NewsEventsPage component with initial layout structure

--- src/pages/NewsEventsPage.tsx
+++ src/pages/NewsEventsPage.tsx
@@ -1,1 +1,9 @@
-  <div className="min-h-screen bg-black"> 
+import React from 'react';
+
+const NewsEventsPage = () => (
+  <div className="min-h-screen bg-black pt-16">
+    {/* Page content goes here */}
+  </div>
+);
+
+export default NewsEventsPage; 
