510e25c74ab7acc978f46987119df95c5abb0f16
Emmanuel D. Humbling <emmergraix@gmail.com>
2025-06-08 10:56:29 +0000

    Add route for SHSDatabasePage in App component to enhance navigation and access to school selection resources.

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,7 @@ import NotFoundPage from './pages/NotFoundPage';
 import PayPalDonatePage from './pages/PayPalDonatePage';
 import DonateUSBankPage from './pages/DonateUSBankPage';
 import DonateUKBankPage from './pages/DonateUKBankPage';
+import SHSDatabasePage from './pages/SHSDatabasePage';
 
 
 
@@ -141,6 +142,7 @@ const App: React.FC = () => {
           <Route path="donate-paypal" element={<PayPalDonatePage />} />
           <Route path="donate-us-bank" element={<DonateUSBankPage />} />
           <Route path="donate-uk-bank" element={<DonateUKBankPage />} />
+          <Route path="shs-database" element={<SHSDatabasePage />} />
 
           {/* Legal Pages */}
           <Route path="privacy-policy" element={<PrivacyPolicyPage />} />
