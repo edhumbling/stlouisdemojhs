2f836f4b40dfacdc9caf8dd9cb426dbe394bdb6f
Emmanuel D. Humbling <emmergraix@gmail.com>
2025-07-13 15:30:43 +0000

    Update advice speeches layout to 2-grid on mobile and add Mellody Hobson TED talk content

--- src/pages/AdviceSpeechesPage.tsx
+++ src/pages/AdviceSpeechesPage.tsx
@@ -160,7 +160,7 @@ const AdviceSpeechesPage: React.FC = () => {
           </div>
 
           {/* Speeches Grid - Apple Style Cards */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
             {commencementSpeeches.map((speech, index) => (
               <motion.div
                 key={speech.id}
